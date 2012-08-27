3e366a2cd0a25d9b6bd66908d29487d118349461
Sumit Chachra <sumit@tivix.com>
2012-08-27 15:51:59 -0700

    Version bump to 0.2. Bug fixes, DEFAULT_LOCK_TIME setting, exact time run capability added etc.

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ f.close()
 
 setup(
     name='django-cron',
-    version='0.1.1',
+    version='0.2',
     author='Sumit Chachra',
     author_email='chachra@tivix.com',
     url='http://github.com/tivix/django-cron',
