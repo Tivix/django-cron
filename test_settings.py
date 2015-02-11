abf5d41b28af9683538ceda976c715f48cb73a16
Dariusz Fryta <dariuszfryta@gmail.com>
2015-02-11 08:54:03 +0100

    Used SQLite for test rather then MySQL

--- test_settings.py
+++ test_settings.py
@@ -1,7 +1,7 @@
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.sqlite3',
         'USER': 'travis',
         'NAME': 'djangocron',
         'TEST_NAME': 'djangocron_test',
