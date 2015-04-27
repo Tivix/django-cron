7de9b158fb3bf7c0e2c023abd2086c86affa832a
mjaworski <inferniac@gmail.com>
2015-04-27 13:37:11 +0200

    rearranged to run different DBs in separate jobs

--- runtests.py
+++ runtests.py
@@ -1,39 +1,22 @@
 # This file mainly exists to allow python setup.py test to work.
 import os
 import sys
-from test_util import reload_settings
 
+if 'DJANGO_SETTINGS_MODULE' not in os.environ:
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'test_settings'
 
 test_dir = os.path.dirname(__file__)
 sys.path.insert(0, test_dir)
 
-
-SETTING_MODULES = (
-    'settings_sqllite',
-    'settings_postgres',
-    'settings_mysql',
-)
+import django
+from django.test.utils import get_runner
+from django.conf import settings
 
 
 def runtests():
-    failures = 0
-    for module in SETTING_MODULES:
-        os.environ['DJANGO_SETTINGS_MODULE'] = module
-        import django
+    TestRunner = get_runner(settings)
+    test_runner = TestRunner(verbosity=1, interactive=False)
+    if hasattr(django, 'setup'):
         django.setup()
-
-        from django.test.utils import get_runner
-        from django.conf import settings
-        from django.core.cache import cache
-
-        reload_settings(settings)
-
-        TestRunner = get_runner(settings)
-        test_runner = TestRunner(verbosity=1, interactive=False)
-        if hasattr(django, 'setup'):
-            django.setup()
-        failures += test_runner.run_tests(['django_cron'])
-
-        cache.clear()
-
+    failures = test_runner.run_tests(['django_cron'])
     sys.exit(bool(failures))
