da32bdc5b1d80fb1cb7c8a3c6298a76ed39e2b49
Rehmat Alam <contact@rehmat.works>
2022-01-16 22:43:15 +0500

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -1,4 +1,7 @@
-from django.utils.translation import ugettext_lazy as _
+try:
+    from django.utils.translation import gettext_lazy as _
+except ImportError:
+    from django.utils.translation import ugettext_lazy as _
 
 
 def humanize_duration(duration):
