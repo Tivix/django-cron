2a9289098a583c2048fa4b252bd3f6d403bf6805
Jedrzej Maluszczak <jedrzej@Jedrzejs-MacBook-Pro.local>
2022-05-10 14:23:54 +0200

    Update docs/sample_cron_configurations and package version

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ f.close()
 
 setup(
     name='django-cron',
-    version='0.5.1',
+    version='0.6.0',
     author='Sumit Chachra',
     author_email='chachra@tivix.com',
     url='http://github.com/tivix/django-cron',
