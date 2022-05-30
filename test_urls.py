648e251d6950933e9ea989e5bd657c32ec23242f
Jedrzej <jedrzej.maluszczak@gmail.com>
2022-05-30 16:35:37 +0200

    fix tests

--- test_urls.py
+++ test_urls.py
@@ -1,9 +1,9 @@
 # urls.py
-from django.conf.urls import url
 from django.contrib import admin
+from django.urls import re_path
 
 admin.autodiscover()
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    re_path(r'^admin/', admin.site.urls),
 ]
