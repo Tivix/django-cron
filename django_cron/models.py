8ffa460c925d9630de60379b6ce25f25b3fc3e88
Frank Meyer <meyer@plexlog.de>
2024-01-09 15:03:31 +0100

    django compatibility

--- django_cron/models.py
+++ django_cron/models.py
@@ -20,9 +20,9 @@ class CronJobLog(models.Model):
         return '%s (%s)' % (self.code, 'Success' if self.is_success else 'Fail')
 
     class Meta:
-        index_together = [
-            ('code', 'is_success', 'ran_at_time'),
-            ('code', 'start_time', 'ran_at_time'),
-            ('code', 'start_time')  # useful when finding latest run (order by start_time) of cron
+        indexes = [
+            models.Index(fields=('code', 'is_success', 'ran_at_time')),
+            models.Index(fields=('code', 'start_time', 'ran_at_time')),
+            models.Index(fields=('code', 'start_time'))  # useful when finding latest run (order by start_time) of cron
         ]
         app_label = 'django_cron'
